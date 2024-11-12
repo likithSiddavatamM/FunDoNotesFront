ffe1930ba24d56ec4863c08a33d0a88728a61e27
LIKITH <likith.isekit2020@gmail.com>
2024-11-13 00:21:25 +0530

    [LIKITH] : Added : Added SignUp functionality component in ReactApp

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App, {SignUp} from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <SignUp/>
   </React.StrictMode>
 );
 
