cd4e1f7bd2895dda60b7b57bf7b3e82327a8d76b
LIKITH <likith.isekit2020@gmail.com>
2024-11-13 10:51:32 +0530

    [LIKITH] : Update : Update SignUp UI with images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import SignUpImg1 from './SignUp.png'
 import './App.css';
 import './FunDoNotesSignUp.css'
 import './FunDoNotesSignIn.css'
@@ -29,10 +30,10 @@ export function SignUp() {
           <p className="signup-password-instruction">Use 8 or more characters with a mix of letters, numbers & symbols</p>
           <button type="submit" className="signup-submit-button">Register</button>
         </form>
-        <a href="../FunDoNotesSignIn/FunDoNotesSignIn.html" className="signup-sign-in-link">Sign in instead</a>
+        <a href="./FunDoNotesSignIn.html" className="signup-sign-in-link">Sign in instead</a>
       </div>
       <div className="signup-info-box">
-        <img src="SignupImg.png" alt="User Icon" style={{ width: '250px', height: 'auto' }} />
+        <img src={SignUpImg1} alt="User Icon" style={{ width: '250px', height: 'auto' }} />
         <p>One account. All of Fundo working for you.</p>
       </div>
     </div>
